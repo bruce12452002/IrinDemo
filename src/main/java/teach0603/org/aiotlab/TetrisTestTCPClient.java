a8f8b2f30bb27028d29160e2ef142cf4db156633
bruceChen <bruce@bruceChendeMacBook-Pro.local>
2023-05-30 16:22:48 +0800

    resolve final @Test

--- src/main/java/teach0603/org/aiotlab/TetrisTestTCPClient.java
+++ src/main/java/teach0603/org/aiotlab/TetrisTestTCPClient.java
@@ -50,6 +50,7 @@ public class TetrisTestTCPClient {
 
     public void start() throws Exception {
         // Fill-in the correct command start
+        sendRequest("start");
     }
 
     public void move(int x) throws Exception {
@@ -79,7 +80,7 @@ public class TetrisTestTCPClient {
         isGameOver = (input.readByte() == (byte)1);
 
         byte [] intBuf = new byte[4];
-        input.read(intBuf, 0, 4);
+        int read = input.read(intBuf, 0, 4);
         linesRemoved = byteToint(intBuf);
 
         input.read(intBuf, 0, 4);
